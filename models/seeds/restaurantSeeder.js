5a2da24eacadd9821270b010b1c0e6813ef58c47
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-09-20 13:16:40 +0800

    refactor:改用insertMany取代creat新增種子資料到mongoodb

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -10,20 +10,8 @@ db.on('error', ()=>{
 })
 db.once('open', ()=>{
     console.log('mongodb connected!')
-    const restaurants = restaurantData.results
-    restaurants.forEach(item => {
-        restaurant.create({
-            "name": item.name,
-            "name_en": item.name_en,
-            "category": item.category,
-            "image": item.image,
-            "location": item.location,
-            "phone": item.phone,
-            "google_map": item.google_map,
-            "rating": item.rating,
-            "description": item.description
-        })
-    });
+    restaurant.insertMany(restaurantData.results)
+    console.log('create seed data!')
 })
 
 
