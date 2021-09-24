42a7218e44a1a7d4548eeeb6b2339a155005a5cd
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-09-25 00:14:09 +0800

    feat:使用multer上傳圖片，並將圖片存放在imgur，取得網址存入資料庫中

--- routes/modules/restaurant.js
+++ routes/modules/restaurant.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const restaurant = require('../../models/restaurant')
+const multer = require('multer')
+const fs = require('fs')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = 'd99dd1de304981e'
+
+//上傳檔案設定
+const upload = multer({ dest: 'temp/' })
 
 //個別餐廳詳細資訊
 router.get('/:id/detail', (req, res) => {
@@ -42,10 +49,27 @@ router.get('/create', (req, res) => {
 })
 
 //接收新增的餐廳資料
-router.post('/create', (req, res) => {
-    restaurant.create(req.body)
-    .then(()=> res.redirect('/'))
-    .catch(error => console.log(error))
+router.post('/create', upload.single('image'), (req, res) => {
+    const { file } = req
+    fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        imgur.setClientID(IMGUR_CLIENT_ID);
+        imgur.upload(file.path, (err, img) => {
+              return restaurant.create({
+                name: req.body.name,
+                name_en: req.body.name_en,
+                category: req.body.category,
+                image: img.data.link,
+                location: req.body.location,
+                phone: req.body.phone,
+                google_map: req.body.google_map,
+                rating: req.body.rating,
+                description: req.body.description
+            })
+            .then(()=> res.redirect('/'))
+            .catch(error => console.log(error))
+        })
+    })
 })
 
 module.exports = router
