ac2c36323b731606542a986a413c0f8ac330dd6b
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-10-24 07:45:08 +0800

    feat:當登入失敗時，利用passport的flash傳遞錯誤訊息到頁面上，fix:google登入callback函式寫錯，從done改成cd

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ app.use((req, res, next) => {
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
     res.locals.success_msg = req.flash('success_msg')  
-    res.locals.warning_msg = req.flash('warning_msg')  
+    res.locals.warning_msg = req.flash('warning_msg')
+    res.locals.failureFlash_msg = req.flash('error')
     next()
 })
 
--- config/passport.js
+++ config/passport.js
@@ -11,15 +11,16 @@ module.exports = app => {
     app.use(passport.session())
     
     //設定本地登入策略
-    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }
+    , (req, email, password, done) => {
         User.findOne({ email })
         .then(user => {
             if (!user) {
-                return done(null, false, { message: 'That email is not registered!' })
+                return done(null, false, { message: '此電子郵件並未被註冊!!' })
             }
             return bcrypt.compare(password, user.password)
             .then(isMath => {
-                if (!isMath) return done(null, false, { message: 'Email or Password incorrect.' })
+                if (!isMath) return done(null, false, { message: '密碼輸入錯誤，請重新輸入!!' })
                 return done(null, user)
             })
         })
@@ -51,7 +52,6 @@ module.exports = app => {
                 .catch(err => done(err, false))
             })
         })
-        .catch(err => done(err, false))
     }))
 
     //設定Google登入策略
@@ -74,8 +74,8 @@ module.exports = app => {
                     email,
                     password: hash
                 })
-                .then(()=> done(null, user))
-                .catch(err => done(err, false))
+                .then(()=> cd(null, user))
+                .catch(err => cd(err, false))
             })
         })
       }
