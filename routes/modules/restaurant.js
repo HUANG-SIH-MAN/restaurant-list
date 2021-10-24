b4c42f59f09d138ad19ea339e0a043e05beced0b
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-10-24 10:35:05 +0800

    feat:README新增種子資料介紹，fix:移動餐廳路由位置，避免新增餐廳資料路由跑不出來

--- routes/modules/restaurant.js
+++ routes/modules/restaurant.js
@@ -9,6 +9,38 @@ const IMGUR_CLIENT_ID = 'd99dd1de304981e'
 //上傳檔案設定
 const upload = multer({ dest: 'temp/' })
 
+//新增餐廳資料    
+router.get('/create', (req, res) => {
+    res.render('create')
+})
+
+//接收新增的餐廳資料
+router.post('/create', upload.single('image'), (req, res) => {
+    const { file } = req
+    const {name, name_en, category, location, phone, google_map, rating, description} = req.body
+    const userId = req.user._id
+    fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        imgur.setClientID(IMGUR_CLIENT_ID);
+        imgur.upload(file.path, (err, img) => {
+              return restaurant.create({
+                name,
+                name_en,
+                category,
+                image: img.data.link,
+                location,
+                phone,
+                google_map,
+                rating,
+                description,
+                userId
+            })
+            .then(()=> res.redirect('/'))
+            .catch(error => console.log(error))
+        })
+    })
+})
+
 //個別餐廳詳細資訊
 router.get('/:id', (req, res) => {
     const id = req.params.id
@@ -41,38 +73,6 @@ router.put('/:id', (req, res) => {
     restaurant.findByIdAndUpdate(id, { $set: req.body })
     .then(()=> res.redirect(`/restaurant/${id}`))
     .catch(error => console.log(error))
-})
-
-//新增餐廳資料    
-router.get('/create', (req, res) => {
-    res.render('create')
-})
-
-//接收新增的餐廳資料
-router.post('/create', upload.single('image'), (req, res) => {
-    const { file } = req
-    const {name, name_en, category, location, phone, google_map, rating, description} = req.body
-    const userId = req.user._id
-    fs.readFile(file.path, (err, data) => {
-        if (err) console.log('Error: ', err)
-        imgur.setClientID(IMGUR_CLIENT_ID);
-        imgur.upload(file.path, (err, img) => {
-              return restaurant.create({
-                name,
-                name_en,
-                category,
-                image: img.data.link,
-                location,
-                phone,
-                google_map,
-                rating,
-                description,
-                userId
-            })
-            .then(()=> res.redirect('/'))
-            .catch(error => console.log(error))
-        })
-    })
 })
 
 module.exports = router
