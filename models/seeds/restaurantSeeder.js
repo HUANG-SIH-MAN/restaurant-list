b5fa356edc4ad59afbccd3f89acec3bf39d47ce3
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-10-24 09:23:48 +0800

    feat:修改種子資料，讓使用者與餐廳資料做連結(未完成process.exit()，需手動離開)

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,13 +1,41 @@
-const restaurant = require('../restaurant') // 載入餐廳 model
-const restaurantData = require('./restaurantData') // 載入餐廳 model
+//載入套件
+const bcrypt = require('bcryptjs')
+
+// 載入 model
+const Restaurant = require('../restaurant') 
+const User = require('../user')
+
+// 載入json資料
+const restaurantData = require('./restaurantData').results
+const userData = require('./userData').results
 
 //設定資料庫連線
 const db = require('../../config/mongoose')
 
 db.once('open', ()=>{
     console.log('mongodb connected!')
-    restaurant.insertMany(restaurantData.results)
-    console.log('create seed data!')
+    Promise.all(Array.from(userData, user => {
+        return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(user.password, salt))
+        .then(hash => {
+            User.create({
+                name: user.name,
+                email: user.email,
+                password: hash
+            })
+            .then(seedUser => {
+                const userId = seedUser._id
+                Promise.all(Array.from(user.restaurant_index, index => {
+                    restaurantData[index].userId = userId
+                    Restaurant.create(restaurantData[index])
+                }))
+            })
+        })
+    }))
+    .then(()=>{
+        console.log('create seed data!')
+        //process.exit()
+    })
+    .catch(err => console.log(err))
 })
-
-
